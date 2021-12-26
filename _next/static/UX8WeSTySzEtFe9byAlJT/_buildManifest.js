3000897cc0c8468e49af18cd20a5fd3978dde9ff
yuhyun lee <dbgus101@gmail.com>
2021-12-26 16:38:56 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/UX8WeSTySzEtFe9byAlJT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/187-26866f636f0d6a32.js","static/chunks/pages/index-b39d19342ef716a3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
