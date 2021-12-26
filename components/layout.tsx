3e6d4d0f49534d0e2f31c894f0441e3de4445a41
yuhyun lee <dbgus101@gmail.com>
2021-12-26 14:47:10 +0900

    start project

---
+++ components/layout.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+
+const Layout = (children: { children: ReactNode }) => {
+  return (
+    <div>
+      <Head>
+        <style jsx global>{`
+          html,
+          body {
+            margin: 0px;
+            padding: 0px;
+            font-family: "Spoqa Han Sans Neo", "sans-serif";
+            font-weight: 500;
+
+            min-width: 0;
+            min-height: 0;
+          }
+          button {
+            font-family: "Spoqa Han Sans Neo", "sans-serif";
+          }
+          input:focus::-webkit-input-placeholder {
+            opacity: 0;
+          }
+        `}</style>
+      </Head>
+      <body>{children}</body>
+      <footer> this is footer..</footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* 폰트는 글로벌리 적용이 필요하기 때문에, 공식 문서에 적혀 있는데로 하는 것 보다 이처럼 하는게 더 좋아보임. */}
+          <link
+            href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
